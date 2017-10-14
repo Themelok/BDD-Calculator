55567fbd4a959603a6fba4dc039ebcadd1f049f1
themelok <3038686@gmail.com>
2017-10-14 22:21:45 +0300

    debuging deleted

--- environment.py
+++ environment.py
@@ -8,20 +8,17 @@ def before_all(context):
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE)
 
-    def handle_response(x):
-        resp = x.decode('utf-8').replace('>','').strip()
-        if chr(8722) in resp: 
-            resp = resp.replace(chr(8722),chr(45))
-        if ',' in resp:
-            resp = resp.replace(',','.')
-            resp = str(round(float(resp),1))
-        return resp
+    def handle_response(response):
+        handled = response.decode('utf-8').replace('>','').strip()
+        if chr(8722) in handled: 
+            handled = handled.replace(chr(8722),chr(45))
+        if ',' in handled:
+            handled = handled.replace(',','.')
+            handled = str(round(float(handled),1))
+        return handled
 
     context.open_proc = open_proc
     context.handle_response = handle_response
-
-# def after_all(context):
-#     context.calc.kill()
 
 def after_scenario(context, scenario):
     context.calc.terminate()
--- steps/steps.py
+++ steps/steps.py
@@ -82,24 +82,18 @@ def step_impl(context):
 
 @when('we divide first numbers by the second')
 def step_impl(context):
-    # import pdb; pdb.set_trace()
     context.calc = context.open_proc()
     msg = '{}/{}'.format(context.first_int, context.second_int)
     context.out, context.err = context.calc.communicate(msg.encode('utf-8'))
     context.draft = str(round(context.first_int/context.second_int,1))
     context.out = context.handle_response(context.out)
-    a=1+1
-    b=1+1
 
 @when('we divide first numbers by zero')
 def step_impl(context):
-    # import pdb; pdb.set_trace()
     context.calc = context.open_proc()
     msg = '{}/{}'.format(context.first_int, context.second_int)
     context.out, context.err = context.calc.communicate(msg.encode('utf-8'))
     context.err = context.handle_response(context.err)
-    a=1+1
-    b=1+1
 
 @then('the result should be equal to quotient')
 def ster_impl(context):
